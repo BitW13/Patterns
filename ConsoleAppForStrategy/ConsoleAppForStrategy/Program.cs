0aef8edf5930db12b9310b21a6b0b3c4e6063f06
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2019-05-03 17:29:54 +0300

    strategy is done

---
+++ ConsoleAppForStrategy/ConsoleAppForStrategy/Program.cs
@@ -0,0 +1,27 @@
+﻿using StrategyPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATCContext context = new ATCContext();
+            context.Strategy = new UsualRateStrategy();
+            context.Request();
+            context.Strategy = new PreferentialRateStrategy();
+            context.Request();
+
+            foreach(var rate in context.Rates)
+            {
+                Console.WriteLine(rate);
+            }
+
+        }
+    }
+}
---
+++ StrategyPattern/StrategyPattern/ATCContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class ATCContext
+    {
+        public List<string> Rates { get; set; }
+
+        public ATCContext()
+        {
+            Rates = new List<string>();
+        }
+
+        public IStrategy Strategy { get; set; }
+        public void Request()
+        {
+            Strategy.AddRate(this);
+        }
+    }
+}
